17041ca43d48bab3cb05255b104c848c5aa5e225
mayaahmedabdelsameaa <mayarahmed8624@gmail.com>
2023-02-05 19:45:31 +0200

    fixing the toggle button

--- js/main.js
+++ js/main.js
@@ -35,6 +35,7 @@
     backSpeed: 100,
     loop: true,
   });
+
   /**
    * nav active li
    */
@@ -55,4 +56,27 @@
       }
     });
   };
+  /**
+   * toogle icon, display the navbar 
+  */
+  let toggle = document.querySelector(".toggle-icon");
+  let header = document.querySelector("#header");
+  let main = document.querySelector("#main");
+  let hero = document.querySelector("#hero");
+  let close = document.querySelector(".toggle-icon-1");
+
+  toggle.addEventListener("click",()=>{
+    header.style.left = "0rem";
+    main.style.left = "20rem";
+    hero.style.left = "20rem";
+    toggle.style.display= "none"
+    close.style.display= "inline-block"
+  });
+  close.addEventListener("click",()=>{
+    header.style.left = "-20rem";
+    main.style.left = "0rem";
+    hero.style.left = "0rem";
+    close.style.display="none";
+    toggle.style.display= "inline-block"
+  });
 })();
